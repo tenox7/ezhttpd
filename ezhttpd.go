13cc2d3014c26340f43edb147c41f579e30b20aa
Antoni Sawicki <as@tenoware.com>
2025-02-17 01:55:15 -0800

    refactor main

--- ezhttpd.go
+++ ezhttpd.go
@@ -1,42 +1,32 @@
 // Copyright 2019 Seg <seg@haxxed.com>
+// Copyright 2025 Tenox <tenox@tenox.net>
 
 package main
 
-import "flag"
-import "os"
-import "path/filepath"
-import "fmt"
-import "github.com/SegHaxx/ezhttpd/http"
+import (
+	"flag"
+	"log"
 
-func main(){
-	flag.Usage = func() {
-		fmt.Println("Usage:",filepath.Base(os.Args[0]),"[options] [path to share]")
-		flag.PrintDefaults()
-	}
-	bind := flag.String("bind",":9000","Bind address")
+	"github.com/SegHaxx/ezhttpd/http"
+)
+
+var (
+	port = flag.String("port", "8080", "port to bind to")
+	path = flag.String("path", "/", "path to serve")
+)
+
+func main() {
 	flag.Parse()
-
-	webroot := flag.Arg(0)
-	if webroot == "" {
-		tmp, err := os.Getwd()
-		if err != nil {return}
-		webroot = tmp
+	log.Printf("Starting ezhttpd on port: %v path: %v", *port, *path)
+	http.Handle("/", http.FileServer(http.Dir(*path)))
+	err := http.ListenAndServe(
+		":"+*port,
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.DefaultServeMux.ServeHTTP(w, r)
+			log.Println(r.RemoteAddr, r.Method, r.URL.Path)
+		}),
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
-	webroot = filepath.Clean(webroot)
-
-	hostname := ""
-	if (*bind)[0] == ':' {
-		tmp, err := os.Hostname()
-		if err == nil {
-			hostname = tmp
-	}}
-
-	fmt.Print("Sharing ",webroot," on http://",hostname,*bind,"/\n")
-
-	http.Handle("/", http.FileServer(http.Dir(webroot)))
-	fmt.Println(http.ListenAndServe((*bind),
-		http.HandlerFunc(func(w http.ResponseWriter,r *http.Request){
-			http.DefaultServeMux.ServeHTTP(w,r)
-			fmt.Println(r.RemoteAddr,r.Method,r.URL.Path)
-	})))
 }
